0f126c8d93146cfa8ff50ae5fc524fc791c46788
sneaky <kanetaylor02@gmail.com>
2024-04-25 23:09:00 +0800

    add homepage information and philippines map svg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,28 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "island-background": "url('../assets/images/background.png')"
       },
+      fontFamily: {
+        'sf-black': ['SF-Black'],
+        'sf-black-italic': ['SF-Black-Italic'],
+        'sf-bold': ['SF-Bold'],
+        'sf-bold-italic': ['SF-Bold-Italic'],
+        'sf-heavy': ['SF-Heavy'],
+        'sf-heavy-italic': ['SF-Heavy-Italic'],
+        'sf-light': ['SF-Light'],
+        'sf-light-italic': ['SF-Light-Italic'],
+        'sf-medium': ['SF-Medium'],
+        'sf-medium-italic': ['SF-Medium-Italic'],
+        'sf-regular': ['SF-Regular'],
+        'sf-regular-italic': ['SF-Regular-Italic'],
+        'sf-semibold': ['SF-Semibold'],
+        'sf-semibold-italic': ['SF-Semibold-Italic'],
+        'sf-thin': ['SF-Thin'],
+        'sf-thin-italic': ['SF-Thin-Italic'],
+        'sf-ultralight': ['SF-Ultralight'],
+        'sf-ultralight-italic': ['SF-Ultralight-Italic']
+      }
     },
   },
   plugins: [],
