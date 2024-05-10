640c664fb25223ea2f0e49a73abb7c9830d7d904
Jade <Jadesuuu@gmail.com>
2024-05-10 20:29:21 +0800

    initial login ui

---
+++ login/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Styles from './page.module.css';
+import Image from 'next/image'
+
+const loginPage = () => {
+  return (
+    <div className={Styles.page}>
+        <div className={Styles.wrapper}>
+        <form action="">
+            <h1>Login</h1>
+            <div className={Styles.inputBox}>
+                <input type="text" placeholder='Username' required/>
+                <Image src={'/user.svg'} alt={'Icon image'} width={32} height={32} className={Styles.icon}/>
+            </div>
+            <div className={Styles.inputBox}>
+                <input type="password" placeholder='Password' required/>
+                <Image src={'/lock.svg'} alt={'Icon image'} width={32} height={32} className={Styles.icon}/>
+            </div>
+            <div className={Styles.rememberForgot}>
+                <label><input type="checkbox" />Remember me</label>
+                <a href="#">Forgot password?</a>
+            </div>
+            <button type='submit' className={Styles.loginButton}>Login</button>
+            <div className={Styles.registerLink}>
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </div>
+        </form>
+    </div>
+    </div>
+  )
+}
+
+export default loginPage
