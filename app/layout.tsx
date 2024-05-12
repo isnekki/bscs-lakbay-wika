957b0f826747b5630a5bf58f5d9d7351650b67d9
Jade <Jadesuuu@gmail.com>
2024-05-12 23:31:16 +0800

    added login and register ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,43 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import { getServerSession } from "next-auth";
-const inter = Inter({ subsets: ["latin"] });
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import NavBar from "./components/NavBar";
+// import Footer from "./components/Footer";
+// import { getServerSession } from "next-auth";
+// import Head from 'next/head'
+// import Logout from "./logout";
+// import Link from 'next/link'
+// const inter = Inter({ subsets: ["latin"] });
 
+
+// export const metadata: Metadata = {
+//   title: "PinaSpeak",
+//   description: "PinaSpeak Homepage",
+// };
+
+// export default async function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   const session = await getServerSession(); //check if there's a session. Create if !!session show logout button if !session a href="/login"
+//   return (
+//     <html lang="en">
+//       <NavBar/>
+//       <div className={inter.className}>{children}</div>
+//       <Footer/>
+//     </html>
+//   );
+// }
+
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import Logout from './logout';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "PinaSpeak",
@@ -14,15 +46,19 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const session = await getServerSession(); //check if there's a session. Create if !!session show logout button if !session a href="/login"
+}) {
+  const session = await getServerSession();
   return (
     <html lang="en">
-      <NavBar/>
-      <div className={inter.className}>{children}</div>
-      <Footer/>
+      <body className={inter.className}>
+        <nav>
+          {!!session && <Logout />}
+          {!session && <Link href="/login">Login</Link>}
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/login/form.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { FormEvent } from "react";
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation";
+import Image from 'next/image'
+
+export default function Form() {
+    const router = useRouter();
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const response = await signIn('credentials', {
+            email: formData.get('email'),
+            password: formData.get('password'),
+            redirect: false,
+        });
+
+        console.log({ response });
+        //handle login error
+        if (!response?.error) {
+            router.push("/"); //route if successful
+            router.refresh(); //refresh cache
+        }
+    }
+    return (
+        <div className="h-screen w-screen flex items-center justify-center bg-island-background bg-[#173f2a] bg-cover bg-blend-multiply">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-2 mx-auto w-96 mt-10 h-96 justify-center bg-transparent border-2.5 rounded-2xl border-white">
+            <h1 className="flex items-center justify-center text-3xl font-sf-semibold" >Login</h1>
+                <div className="relative w-full h-12 mt-7 px-4">
+                    <input name="email" className="w-full h-12 bg-transparent border-2.5 rounded-full border-white px-3 placeholder-white pr-10"  type="email" placeholder="Email"/>
+                    <Image src={'/user.svg'} alt={'Icon image'} width={32} height={32} className="absolute text-base translate-x-1/2 right-10 top-2"/>
+                </div>
+                <div className="relative w-full h-12 mt-7 px-4">
+                    <input name="password" className="w-full h-12 bg-transparent border-2.5 rounded-full border-white px-3 placeholder-white pr-10" type="password" placeholder="Password"/>
+                    <Image src={'/lock.svg'} alt={'Icon image'} width={32} height={32} className="absolute translate-x-1/2 right-10 top-2"/>
+                </div>
+                <div className="flex justify-between mx-5 mt-2">
+                    <label className="text-xs"><input className="mx-1" type="checkbox" />Remember me</label>
+                    <a className="text-xs" href="#">Forgot password?</a>
+                </div>
+                <div className="relative w-full h-12 mt-4 px-4">
+                    <button className="w-full h-12 bg-white outline-none border-none rounded-full text-black font-sf-semibold hover:bg-slate-100" type='submit'>Login</button>
+                </div>
+                <div className="flex justify-center">
+                    <p className="text-sm">Don't have an account? <a className="text-sm font-sf-semibold hover:underline" href="/register">Register</a></p>
+                </div>
+            </form>
+        </div>
+    )
+}
